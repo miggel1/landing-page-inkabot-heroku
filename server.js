974fbd2547ad3688a370484ac510df1861d76e4f
Miguel Miranda <jmmiranda.c@gmail.com>
2021-05-23 20:33:19 -0500

    heroku 4

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 //app.use(express.static(__dirname + '/dist/landing-page-inkabot-heroku'));
-app.use(express.static(path.join(__dirname, 'dist','landing-page-inkabot-heroku')));
+app.use(express.static(__dirname + '/dist/landing-page-inkabot-heroku'));
 
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname,'dist','landing-page-inkabot-heroku','index.html'));
+    res.sendFile(path.join(__dirname + '/dist/landing-page-inkabot-heroku/index.html'));
     /*res.sendFile(path.join(__dirname + 
         '/dist/landing-page-inkabot-heroku/index.html'));*/
 });
