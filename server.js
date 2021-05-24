bdf96bdf698453d543b705f73d5aaee799da281f
Miguel Miranda <jmmiranda.c@gmail.com>
2021-05-23 21:24:32 -0500

    heroku 7

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
-//Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
-
-// Serve only the static files form the dist directory
-//app.use(express.static(__dirname + '/dist/landing-page-inkabot-heroku'));
-app.use(express.static('./dist/landing-page-inkabot-heroku'));
-
-
-app.get('/*', function(req, res){
-    res.sendFile('index.html', {root: 'dist/landing-page-inkabot-heroku'}
-    );
-    /*res.sendFile(path.join(__dirname + 
-        '/dist/landing-page-inkabot-heroku/index.html'));*/
-});
-
-
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/dist/landing-page-inkabot-heroku'));
 app.listen(process.env.PORT || 8080);
 
-console.log(`Running on port ${process.env.PORT || 8080}`)
+//PATH LOCATION STARTEGY
+
+app.get('/*', function(req,res){
+  const fullPath = path.join(__dirname + '/dist/landing-page-inkabot-heroku/index.html');
+  console.log(" Fetching from.." + fullPath);
+    res.sendFile(fullPath);
+})
+
+console.log('Server started running..');
