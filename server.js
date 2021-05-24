12e559ebcde662763060845a1e4f6341fdfa2865
Miguel Miranda <jmmiranda.c@gmail.com>
2021-05-23 20:40:42 -0500

    heroku 5

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 //app.use(express.static(__dirname + '/dist/landing-page-inkabot-heroku'));
-app.use(express.static(__dirname + '/dist/landing-page-inkabot-heroku'));
+app.use(express.static(path.join(__dirname + '/dist/landing-page-inkabot-heroku')));
 
 
 app.get('/*', function(req, res){
