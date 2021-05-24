352c44d900e7c31a834eed3f90f610e095a73063
Miguel Miranda <jmmiranda.c@gmail.com>
2021-05-23 20:07:08 -0500

    heroku 3

--- server.js
+++ server.js
@@ -6,23 +6,15 @@ const app = express();
 
 // Serve only the static files form the dist directory
 //app.use(express.static(__dirname + '/dist/landing-page-inkabot-heroku'));
-app.use(express.static('${__dirname}/front-end/dist/'));
+app.use(express.static(path.join(__dirname, 'dist','landing-page-inkabot-heroku')));
 
-app.get('*', function(req, res){
-    res.sendFile(`./front-end/dist/index.html`);
-    
+
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname,'dist','landing-page-inkabot-heroku','index.html'));
     /*res.sendFile(path.join(__dirname + 
         '/dist/landing-page-inkabot-heroku/index.html'));*/
 });
 
-/*
-app.get('/*', function(req, res){
-    res.sendFile('index.html', { root: 'dist/landing-page-inkabot-heroku/' }
-    );
-    /*res.sendFile(path.join(__dirname + 
-        '/dist/landing-page-inkabot-heroku/index.html'));
-});
-*/ 
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
